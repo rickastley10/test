53f6689b7d3f97a3a28ebb7e74924446d92ffeb6
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2025-06-25 20:02:45 +0300

    test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+while 1 == 1:
+    print('test system update 0.3')
+    x = input('type [exit] to exit \n type [anything] : ')
+    if x == 'anything':
+        print('you typed anything \n litteraly')
+    elif x == 'exit':
+        exit()
+    else:
+        print('you typed ', x)
