05b7863de7e13ad88bc47f2c506fa886ed90e90c
rickastley10 <148710838+rickastley10@users.noreply.github.com>
2025-06-30 14:42:59 +0300

    Update and rename test.html to test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+while 1 == 1:
+    print('choose: rock/ paper / scissors')
+    x = input()
+    y = input()
+    if x == 'test':
+        print('testing complete')
+    print('player 1 choose - ', x, '    and player 2 choose - ', y)
